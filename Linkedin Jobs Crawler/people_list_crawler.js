89905eae8f0c3258d2c7e53dcfd9869fdec042c9
Mohammed Zulfiqar <150642443+zulfiqar-uxd@users.noreply.github.com>
2024-03-20 11:03:21 +0400

    Update people_list_crawler.js

--- Linkedin Jobs Crawler/people_list_crawler.js
+++ Linkedin Jobs Crawler/people_list_crawler.js
@@ -1,3 +1,6 @@
+// STEP 9: OPEN EACH COMAPNY PROFILE LSITED ON THE JSON AND CLICK ON THE CURRENT WORKING PEOPLE'S SECTION
+// STEP 10: RUN THE BELOW SCRIPT ON ALL PAGES OD THE PEOPLE LIST (NOTE: GO SLOW, BEWARE OF USAGE LIMITS)
+
 let people_list = document.querySelectorAll('.reusable-search__result-container');
 if (typeof staffDetails === 'undefined') {staffDetails = [];}
 
@@ -14,3 +17,6 @@ people_list.forEach(people_card => {
 });
 
 console.log(JSON.stringify(staffDetails, null, 2));
+
+// STEP 11: COPY THE FINAL RESULT WITH ALL PEOPLE INTO THE PEOPLE PART OF THE FIRST ARRAY
+// STEP 12: OPEN 'https://crowstacks.com/projects/autojob' AND WORK ON EACH COMPANY/JOB AND MESSAGE EACH PEOPLE LISTED USING THE GENERATED MESSAGE TEMPLATE
